1214d427ddc30a5a1b95c1bf6f46cc77f9637a19
Kenny <keggex101@gmail.com>
2025-08-21 11:37:26 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(num1, num2){
+    return num1 + num2 
+};
+
+function minus(num1, num2){
+    return num1 - num2
+};
+
+function times(num1, num2){
+    return num1 * num2
+};
+
+function divide(num1, num2){
+    return num1 / num2
+};
+
+function operate(operator, num1, num2){
+    if (operator === "+"){
+        return add(num1, num2);
+    }
+    else if (operator === "-"){
+        return minus(num1, num2);
+    }
+    else if (operator ==="*"){
+        return times(num1, num2);
+    }
+    else if (operator === "/"){
+        return divide(num1, num2);
+    }
+    else {
+        console.log("Not the correct operator");
+    }
+};
+
+let op = operate("/", 1, 2);
+console.log(op)
